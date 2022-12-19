7b0cb2759ad294a2ef13a36371520612325dc582
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-12-19 15:42:46 +0000

    Task 0 through 2 of the simple shell project

---
+++ main.c
@@ -0,0 +1,94 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+/**
+ * _printf - Prints a string to standard output
+ * @str: the string to be printed
+ *
+ * Return: the number of characters printed
+ */
+int _printf(char *str)
+{
+	int i = 0, len = 0;
+
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (write(1, str, len));
+}
+/**
+ * fork_shell - Calls fork to run the shell
+ * @str: array of strings with commands and arguments
+ *
+ * Return: nothing
+ */
+void fork_shell(char *str[])
+{
+	pid_t child;
+	int x, wstatus;
+
+	if (str[0])
+	{
+		if (str[0][0] == '^' && (str[0][1] == 'D' || str[0][1] == 'C')
+				&& str[0][2] == '\0')
+		{
+			exit(-1);
+		}
+		child = fork();
+		if (child == -1)
+		{
+			perror("./shell");
+			exit(-1);
+		}
+		if (child == 0)
+		{
+			x = execve(str[0], str, environ);
+			if (x == -1)
+			{
+				perror("./shell");
+				exit(-1);
+			}
+		}
+		else
+			wait(&wstatus);
+	}
+}
+/**
+ * main - Entry Point for the shell program
+ *
+ * Return: 0 on Success, anything else is failure
+ */
+int main(void)
+{
+	char *string[128];
+	char *token, *lineptr = NULL;
+	int i = 1;
+	ssize_t line;
+	size_t n = 0;
+
+	while (1)
+	{
+		_printf("($) ");
+		line = getline(&lineptr, &n, stdin);
+		if (line == -1)
+			exit(-1);
+		token = strtok(lineptr, " \n");
+		string[0] = token;
+		while (token != NULL)
+		{
+			token = strtok(NULL, " \n");
+			string[i] = token;
+			i++;
+		}
+		fork_shell(string);
+		i = 1;
+	}
+	free(lineptr);
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+extern char **environ;
+int _printf(char *str);
+void fork_shell(char *str[]);
+int main(void);
+#endif /* MAIN_H */
